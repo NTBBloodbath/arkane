d089afced7f246bedfeeca7f7bde1f592aeab76a
NTBBloodbath <bloodbathalchemist@protonmail.com>
2022-08-02 02:09:11 -0400

    feat(character.zig): add initial empty file
    chore(src): add copyright header comment

---
+++ src/character.zig
@@ -0,0 +1,5 @@
+// ┌                                                          ┐
+// │  Copyright (c) 2022 NTBBloodbath. All rights reserved.   │
+// │  Use of this source code is governed by a GPLv3 license  │
+// │          that can be found in the LICENSE file.          │
+// └                                                          ┘
--- src/fs.zig
+++ src/fs.zig
@@ -1,3 +1,8 @@
+// ┌                                                          ┐
+// │  Copyright (c) 2022 NTBBloodbath. All rights reserved.   │
+// │  Use of this source code is governed by a GPLv3 license  │
+// │          that can be found in the LICENSE file.          │
+// └                                                          ┘
 const std = @import("std");
 
 /// System paths used by Arkane.
